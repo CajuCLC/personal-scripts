097b9ae0c385795eef2aa142b61d7e6db1062cfa
Eric Cavalcanti <cajuclc@gmail.com>
2015-11-04 11:18:56 -0600

    Update change_awstats.sh

--- change_awstats.sh
+++ change_awstats.sh
@@ -3,5 +3,6 @@ for dominio in $(cat list.txt);
         do
         user=$(echo $dominio | awk '{print substr($0,0,4)}' | cut -d"." -f1)
         domain=$(echo $dominio)
-        echo sed -i "s/AllowAccessFromWebToFollowingAuthenticatedUsers=\"\"/AllowAccessFromWebToFollowingAuthenticatedUsers=\"$user\"/g" /etc/awstats/awstats.$domain.conf
+        sed -i "s/AllowAccessFromWebToFollowingAuthenticatedUsers=\"\"/AllowAccessFromWebToFollowingAuthenticatedUsers=\"$user\"/g" /etc/awstats/awstats.$domain.conf
+        sed -i "s/AllowAccessFromWebToAuthenticatedUsersOnly=0/AllowAccessFromWebToAuthenticatedUsersOnly=1/g" /etc/awstats/awstats.$domain.conf
         done
