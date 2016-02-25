b2bf0aa695023c3789571fb469a7ff9a9e90b2af
Eric Cavalcanti <cajuclc@gmail.com>
2016-02-25 09:54:10 -0600

    Update logformat.sh

--- logformat.sh
+++ logformat.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Getting domains"
-httpd -S | grep "namevhost" | grep "port 80" | awk {' print $4 '} | sort | uniq > domains.txt
+httpd -S | grep "namevhost" | grep "port 80" | awk {' print $4 '} | sort | uniq >> domains.txt
 echo "LogLevel non-SSL"
 for i in $(cat dominios.txt); do sed -i "s/#        LogLevel warn/#LogLevel warn/" /etc/httpd/vhost.d/$i.conf; done
 echo "LogFormat"
-for i in $(cat dominios.txt); do sed -i "s/        LogLevel warn/        LogLevel warn\n        LogFormat \"%h %l %u %t \\\\"\"%r\\\\"\" %>s %b \\\\"\"%{Referer}i\\\\"\" \\\\"\"%{User-Agent}i\\\\"\"\"/" /etc/httpd/vhost.d/$i.conf; done
+for x in $(cat dominios.txt); do sed -i "s/        LogLevel warn/        LogLevel warn\n        LogFormat \"%h %l %u %t \\\\"\"%r\\\\"\" %>s %b \\\\"\"%{Referer}i\\\\"\" \\\\"\"%{User-Agent}i\\\\"\"\"/" /etc/httpd/vhost.d/$x.conf; done
